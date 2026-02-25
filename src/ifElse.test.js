f11e8a5eb86843823f8b8697f29fd20b84e8d655
Vitalii <v.hulaievych@gmail.com>
2026-02-25 14:21:14 +0000

    solution

--- src/ifElse.test.js
+++ src/ifElse.test.js
@@ -1,11 +1,43 @@
 'use strict';
 
 describe('ifElse', () => {
-  // const { ifElse } = require('./ifElse');
+  const { ifElse } = require('./ifElse');
+  let conditionTrue;
+  let conditionFalse;
+  let first;
+  let second;
 
-  it('should ', () => {
-
+  beforeEach(() => {
+    conditionTrue = jest.fn(() => true);
+    conditionFalse = jest.fn(() => false);
+    first = jest.fn();
+    second = jest.fn();
   });
 
-  // write tests here
+  afterEach(() => {
+    conditionTrue.mockRestore();
+    conditionFalse.mockRestore();
+    first.mockRestore();
+    second.mockRestore();
+  });
+
+  it('should return noting', () => {
+    expect(ifElse(conditionTrue, first, second)).toBeUndefined();
+  });
+
+  it('should pass first fuction if condition is true', () => {
+    ifElse(conditionTrue, first, second);
+
+    expect(conditionTrue).toHaveBeenCalledTimes(1);
+    expect(first).toHaveBeenCalledTimes(1);
+    expect(second).not.toHaveBeenCalledTimes;
+  });
+
+  it('should pass first fuction if condition is false', () => {
+    ifElse(conditionFalse, first, second);
+
+    expect(conditionFalse).toHaveBeenCalledTimes(1);
+    expect(first).not.toHaveBeenCalledTimes;
+    expect(second).toHaveBeenCalledTimes(1);
+  });
 });
